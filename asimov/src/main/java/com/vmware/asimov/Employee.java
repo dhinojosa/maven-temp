4aeabb4cac31ae3edc3e50758e57fe3ecba86ec1
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-01-06 22:27:56 -0600

    added asimov bradbury

---
+++ asimov/src/main/java/com/vmware/asimov/Employee.java
@@ -0,0 +1,31 @@
+package com.vmware.asimov;
+
+public class Employee {
+	private String ssn;
+	private String firstName;
+	private String lastName;
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
---
+++ bradbury/src/main/java/com/vmware/App.java
@@ -0,0 +1,16 @@
+package com.vmware;
+
+import com.vmware.asimov.Employee;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		Employee employee = new Employee();
+		employee.setFirstName("Albert");
+		employee.setLastName("Einstein");
+		System.out.println("Hello World!");
+	}
+}
