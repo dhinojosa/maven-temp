6b97e94f4f6f2cd1ffdebf3e75dda83cce5587dc
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2014-01-08 17:10:20 -0600

    add new files from class

--- asimov/src/main/java/com/vmware/asimov/App.java
+++ asimov/src/main/java/com/vmware/asimov/App.java
@@ -1,13 +1,23 @@
 package com.vmware.asimov;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
         System.out.println( "Hello World!" );
+        
+        InputStream is = App.class.getResourceAsStream("/server.properties");
+        int i = 0;
+        while ((i = is.read()) != -1){
+        	System.out.print((char)i);
+        }
     }
 }
